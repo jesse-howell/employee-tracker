85e0a161415afa468f5b4bae461d0559f51cc88e
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-04-01 01:38:19 -0400

    added all queries to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import and require express and mysql
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 // create express app
 const PORT = process.env.PORT || 3001;
@@ -8,39 +9,97 @@ const app = express();
 
 // express middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    // password: 'root',
     database: 'employee_db',
   },
   console.log('Connected to the employee_db database.')
 );
 
-// get all employees
-app.get('/api/employees', (req, res) => {
-  db.query('SELECT * FROM employee', (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
+// query department table in database
+db.query('SELECT * FROM department', function (err, results) {
+  console.log(results);
 });
 
-// get employee by id
-app.get('/api/employees/:id', (req, res) => {
-  db.query('SELECT * FROM employee WHERE id =?', req.params.id, (err, data) => {
+// create department
+db.query('INSERT INTO department (name) VALUES (?)', ['Sales'], (err, data) => {
+  if (err) {
+    res.send(err);
+  } else {
+    res.send(data);
+  }
+});
+
+// update department
+db.query(
+  'UPDATE department SET name =? WHERE id =?',
+  ['Sales', 1],
+  (err, data) => {
     if (err) {
       res.send(err);
     } else {
       res.send(data);
     }
-  });
+  }
+);
+
+// delete department
+db.query('DELETE FROM department WHERE id =?', [1], (err, data) => {
+  if (err) {
+    res.send(err);
+  } else {
+    res.send(data);
+  }
+});
+// query role table in database
+db.query('SELECT * FROM role', function (err, results) {
+  console.log(results);
+});
+
+// create role
+db.query(
+  'INSERT INTO role (title, salary, department_id) VALUES (?,?,?)',
+  ['Sales Lead', 100000, 1],
+  (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  }
+);
+
+// update role
+db.query(
+  'UPDATE role SET title =?, salary =?, department_id =? WHERE id =?',
+  ['Sales Lead', 100000, 1, 1],
+  (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  }
+);
+
+// delete role
+db.query('DELETE FROM role WHERE id =?', [1], (err, data) => {
+  if (err) {
+    res.send(err);
+  } else {
+    res.send(data);
+  }
+});
+
+// query employee table in database
+db.query('SELECT * FROM employee', function (err, results) {
+  console.log(results);
 });
 
 // create employee
@@ -64,57 +123,31 @@ app.post('/api/employees', (req, res) => {
 });
 
 // update employee
-app.put('/api/employees/:id', (req, res) => {
-  db.query(
-    'UPDATE employee SET first_name =?, last_name =?, role_id =?, manager_id =? WHERE id =?',
-    [
-      req.body.first_name,
-      req.body.last_name,
-      req.body.role_id,
-      req.body.manager_id,
-      req.params.id,
-    ],
-    (err, data) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(data);
-      }
+db.query(
+  'UPDATE employee SET first_name =?, last_name =?, role_id =?, manager_id =? WHERE id =?',
+  [
+    req.body.first_name,
+    req.body.last_name,
+    req.body.role_id,
+    req.body.manager_id,
+    req.body.id,
+  ],
+  (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
     }
-  );
-});
+  }
+);
 
 // delete employee
-app.delete('/api/employees/:id', (req, res) => {
-  db.query('DELETE FROM employee WHERE id =?', req.params.id, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-// get all roles
-app.get('/api/roles', (req, res) => {
-  db.query('SELECT * FROM role', (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-// get role by id
-app.get('/api/roles/:id', (req, res) => {
-  db.query('SELECT * FROM role WHERE id =?', req.params.id, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
+db.query('DELETE FROM employee WHERE id =?', [1], (err, data) => {
+  if (err) {
+    res.send(err);
+  } else {
+    res.send(data);
+  }
 });
 
 // create role
@@ -156,75 +189,4 @@ app.delete('/api/roles/:id', (req, res) => {
       res.send(data);
     }
   });
-});
-
-// get all departments
-app.get('/api/departments', (req, res) => {
-  db.query('SELECT * FROM department', (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-// get department by id
-app.get('/api/departments/:id', (req, res) => {
-  db.query(
-    'SELECT * FROM department WHERE id =?',
-    req.params.id,
-    (err, data) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-// create department
-app.post('/api/departments', (req, res) => {
-  db.query(
-    'INSERT INTO department (name) VALUES (?)',
-    req.body.name,
-    (err, data) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-// update department
-app.put('/api/departments/:id', (req, res) => {
-  db.query(
-    'UPDATE department SET name =? WHERE id =?',
-    [req.body.name, req.params.id],
-    (err, data) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-// delete department
-app.delete('/api/departments/:id', (req, res) => {
-  db.query('DELETE FROM department WHERE id =?', req.params.id, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
